5ce2687cf83704515b36a72f828aa3ce7c1fe6c4
guidotti <vinimoraes321@hotmail.com>
2025-07-17 18:21:14 -0300

    chore: remove log

--- preload.js
+++ preload.js
@@ -3,5 +3,3 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   openExternal: (url) => ipcRenderer.send('open-external', url)
 });
-
-console.log('Preload script loaded successfully');
